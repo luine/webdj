febc9bb6e4b42187113c351a4a736d41f74963e2
kuboyama <kuboyama@192.168.1.102>
2011-06-02 21:25:35 +0900

    add response model

---
+++ src/main/java/com/camobile/dig/model/Response.java
@@ -0,0 +1,24 @@
+package com.camobile.dig.model;
+
+import java.lang.reflect.Field;
+
+public class Response {
+
+	private String result;
+	private String uri;
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	
+	
+	
+}
